aea35819516eff745a981bb0823d9632caaa8c09
Joseph <118475783+josephmccomas@users.noreply.github.com>
2023-02-02 11:21:02 -0600

    v1.01

--- js/keys.js
+++ js/keys.js
@@ -1,1 +1,5 @@
 const API_KEY = "249f222afb1002186f4d88b2b5418b55";
+
+const API_SEARCH = `https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&language=en-US&query=`;
+
+const IMAGE_PATH = "https://image.tmdb.org/t/p/w500";
